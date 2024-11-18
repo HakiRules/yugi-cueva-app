59c44c6de3c44c9e8709c2ca7dae33d9436d4809
HakiRules <hakikerrules@gmail.com>
2024-11-18 20:10:20 +0100

    Clear project

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function SettingsScreen() {
 
   return (
     <View>
-      <Text>Home screen</Text>
+      <Text>Settings screen</Text>
     </View>
   )
 }

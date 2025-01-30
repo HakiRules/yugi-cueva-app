af9a8d232592e2a38ae7e0ba3811e8d948a29642
HakiRules <hakikerrules@gmail.com>
2025-01-30 20:45:24 +0100

    Fix flatlist inside scrollview warning

--- app/deck/index.tsx
+++ app/deck/index.tsx
@@ -32,46 +32,50 @@ export default function Page() {
     return prev + current.quantity
   }, 0)
 
+
   return (
     <ScrollView style={{ padding: 10 }}>
       <View>
         <Text>Main Deck ({mainDeckCount} Card Deck)</Text>
       </View>
-      <View style={{ flexDirection: 'row', gap: 3, flexWrap: 'wrap', width: 'auto' }}>
-        {deck[CardPosition.MAIN].map(itm =>
+      <View style={{ flex: 5, flexDirection: 'row', flexWrap: 'wrap', padding: 2 }}>
+        {deck[CardPosition.MAIN].map(item => <View style={{ padding: 2, maxWidth: '20%' }}>
           <Image
-            key={itm.card_id}
-            style={{ height: 108, aspectRatio: 0.68 }}
+            key={item.card_id}
+            style={{ width: '100%', aspectRatio: 0.68, flex: 1 / 5 }}
             resizeMode="contain"
-            source={{ uri: `https://tbdesplqufizydsciqzq.supabase.co/storage/v1/object/public/CardImages/${itm.card_id}.jpg?ver=${timestamp}` }}
+            source={{ uri: `https://tbdesplqufizydsciqzq.supabase.co/storage/v1/object/public/CardImages/${item.card_id}.jpg?ver=${timestamp}` }}
           />
-        )}
+          <Text style={{ position: 'absolute', bottom: 5, right: 5, fontSize: 20, fontWeight: 'bold' }}>x{item.quantity}</Text>
+        </View>)}
       </View>
       <View>
         <Text>Extra Deck</Text>
       </View>
-      <View style={{ flexDirection: 'row', gap: 3, flexWrap: 'wrap' }}>
-        {deck[CardPosition.EXTRA].map(itm =>
+      <View style={{ flex: 5, flexDirection: 'row', flexWrap: 'wrap', padding: 2 }}>
+        {deck[CardPosition.EXTRA].map(item => <View style={{ padding: 2, maxWidth: '20%' }}>
           <Image
-            key={itm.card_id}
-            style={{ height: 108, aspectRatio: 0.68 }}
+            key={item.card_id}
+            style={{ width: '100%', aspectRatio: 0.68, flex: 1 / 5 }}
             resizeMode="contain"
-            source={{ uri: `https://tbdesplqufizydsciqzq.supabase.co/storage/v1/object/public/CardImages/${itm.card_id}.jpg?ver=${timestamp}` }}
+            source={{ uri: `https://tbdesplqufizydsciqzq.supabase.co/storage/v1/object/public/CardImages/${item.card_id}.jpg?ver=${timestamp}` }}
           />
-        )}
+          <Text style={{ position: 'absolute', bottom: 5, right: 5, fontSize: 20, fontWeight: 'bold' }}>x{item.quantity}</Text>
+        </View>)}
       </View>
       <View>
         <Text>Side Deck</Text>
       </View>
-      <View style={{ flexDirection: 'row', gap: 3, flexWrap: 'wrap' }}>
-        {deck[CardPosition.SIDE].map(itm =>
+      <View style={{ flex: 5, flexDirection: 'row', flexWrap: 'wrap', padding: 2 }}>
+        {deck[CardPosition.SIDE].map(item => <View style={{ padding: 2, maxWidth: '20%' }}>
           <Image
-            key={itm.card_id}
-            style={{ height: 108, aspectRatio: 0.68 }}
+            key={item.card_id}
+            style={{ width: '100%', aspectRatio: 0.68, flex: 1 / 5 }}
             resizeMode="contain"
-            source={{ uri: `https://tbdesplqufizydsciqzq.supabase.co/storage/v1/object/public/CardImages/${itm.card_id}.jpg?ver=${timestamp}` }}
+            source={{ uri: `https://tbdesplqufizydsciqzq.supabase.co/storage/v1/object/public/CardImages/${item.card_id}.jpg?ver=${timestamp}` }}
           />
-        )}
+          <Text style={{ position: 'absolute', bottom: 5, right: 5, fontSize: 20, fontWeight: 'bold' }}>x{item.quantity}</Text>
+        </View>)}
       </View>
     </ScrollView>
   )
